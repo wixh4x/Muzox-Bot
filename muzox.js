c037aa6cb6b2580fb1b98d619d60ed3fc68d76d4
W!X h4x <137712998+wixh4x@users.noreply.github.com>
2023-07-07 15:59:03 +0600

    Update shelby.js

--- muzox.js
+++ muzox.js
@@ -1,10 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+ { Client, GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
-	console.log('Muzox is ready m@f !!');
+	console.log('Shelby is ready m@f !!');
 });
 
 client.login(config.token);
